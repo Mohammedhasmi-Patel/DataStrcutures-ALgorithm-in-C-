76a30f1e775463d9da58dcbbeeadc12fc4135cf5
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-01-25 22:29:00 +0530

    complete 4th lec

---
+++ ifelse/marks.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // int marks;
+  // cin >> marks;
+  // if (marks > 33)
+  // {
+  //   cout << "pass";
+  // }
+  // else
+  // {
+  //   cout << "fail";
+  // }
+
+  // int a, b;
+  // cin >> a >> b;
+  // if (a > b)
+  // {
+  //   cout << "yes";
+  // }
+  // else
+  // {
+  //   cout << "no";
+  // }
+
+  int num;
+  cin >> num;
+  if (num % 2 == 0)
+  {
+    cout << "even";
+  }
+  else
+  {
+    cout << "odd";
+  }
+  return 0;
+}
---
+++ ifelse/vowels.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char ch;
+  cin >> ch;
+
+  if (ch == 'a')
+  {
+    cout << "vowels";
+  }
+  else if (ch == 'e')
+  {
+    cout << "vowels";
+  }
+  else if (ch == 'i')
+  {
+    cout << "vowels";
+  }
+  else if (ch == 'o')
+  {
+    cout << "vowels";
+  }
+  else if (ch == 'u')
+  {
+    cout << "vowels";
+  }
+  else
+  {
+    cout << "consonant";
+  }
+
+  return 0;
+}
